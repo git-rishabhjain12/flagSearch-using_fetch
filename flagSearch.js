c3407120c22c7294af7f4f67293b271fd69850f1
rishabhjain12 <rishabhj.1222@gmail.com>
2023-04-13 15:45:56 +0530

    change in JS

--- flagSearch.js
+++ flagSearch.js
@@ -1,21 +1,23 @@
 const input = document.querySelector("input");
 const anchorButton = document.querySelector("a");
+const flagbox = document.querySelector("#box2");
 
 anchorButton.onclick = (e) => {
   e.preventDefault();
-  const flagbox = document.querySelector("#box2");
   flagbox.innerHTML = "";
 
   async function fetchData() {
     const response = await fetch("https://flagcdn.com/en/codes.json");
     const result = await response.json();
-    console.log(result);
+    // console.log(result);
     showFlag(result);
   }
   fetchData();
 };
 
 function showFlag(flags) {
+  let error = 1;
+  flagbox.innerHTML = "";
   const flagName = input.value;
   const flagCapitalize =
     flagName.charAt(0).toUpperCase() + flagName.slice(1).toLowerCase();
@@ -24,7 +26,13 @@ function showFlag(flags) {
     if (value === flagCapitalize) {
       const img = document.createElement("img");
       img.src = "https://flagcdn.com/160x120/" + [key][0] + ".png";
-      document.querySelector("#box2").append(img);
+      flagbox.append(img);
+      error = 0;
     }
+  }
+  if (error === 1) {
+    const span = document.createElement("span");
+    span.innerHTML = "Please check the spelling";
+    flagbox.append(span);
   }
 }
